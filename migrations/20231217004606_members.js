7332b403c48c6fa0dce1d7d51028dcf9b59fef6b
elgaridlo <elga@zero-one-group.com>
2023-12-17 16:32:32 +0700

    feat/1-create-members-and-casdi-appoin-tables

---
+++ migrations/20231217004606_members.js
@@ -0,0 +1,24 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable('members', function (table) {
+        table.string('code').primary();
+        table.string('kid_name').notNullable();
+        table.string('is_asd').defaultTo(true).notNullable();
+        table.string('parent_name').notNullable();
+        table.text('phone_no').notNullable();
+        table.text('email').notNullable;
+        table.timestamp('created_at').defaultTo(knex.fn.now());
+        table.timestamp('updated_at').defaultTo(knex.fn.now());                
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTable('members');
+};
