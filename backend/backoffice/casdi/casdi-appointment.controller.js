6436dbc1fc1666d18edde9feb12c71723a6d0ad7
elgaridlo <elga@zero-one-group.com>
2023-12-21 18:29:41 +0700

    feat: 7 add download members and get schedule that already taken

--- backend/backoffice/casdi/casdi-appointment.controller.js
+++ backend/backoffice/casdi/casdi-appointment.controller.js
@@ -3,6 +3,7 @@ const knex = require('../../config/configuration');
 const AppError = require('../../utils/appError');
 const { startOfDay } = require('date-fns');
 const { durations } = require('./helper');
+const stringify = require('csv-stringify');
 
 const appointmentDateValidate = (body) => {
   const { appointment_date, duration } = body;
@@ -65,6 +66,7 @@ exports.createAppointment = catchAsync(async (req, res, next) => {
   await validating(req);
   await knex('casdi_appointments').insert({
     ...req.body,
+    code_member: appointment_date.toUpperCase(),
   });
 
   const data = await knex
@@ -74,5 +76,78 @@ exports.createAppointment = catchAsync(async (req, res, next) => {
   res.status(201).json({
     status: 'Success',
     data,
+  });
+});
+
+exports.getHourMember = catchAsync(async (req, res, next) => {
+  const { appointment_date } = req.body;
+  if (!appointment_date)
+    return next(new AppError('Tolong isi jadwal kunjungan', 400));
+
+  const appoints = await knex
+    .select('*')
+    .from('casdi_appointments')
+    .where('appointment_date', appointment_date);
+
+  const hourExist = appoints.map((item) => item.duration);
+  const filtered = durations.filter((item) => !hourExist.includes(item));
+
+  res.status(200).json({
+    status: 'Success',
+    data: filtered,
+  });
+});
+
+exports.downloadCasdi = catchAsync(async (req, res) => {
+  const appoints = await knex
+    .select(
+      'phone_no',
+      'code_member',
+      'appointment_date',
+      'kid_name',
+      'is_asd',
+      'parent_name',
+      'email',
+      'duration',
+      'casdi_appointments.created_at',
+    )
+    .from('casdi_appointments')
+    .innerJoin('members', 'members.code', 'casdi_appointments.code_member');
+
+  const formattedAppoints = appoints.map((appoint) => {
+    return {
+      code_member: appoint.code_member,
+      duration: appoint.duration,
+      appointment_date: new Date(appoint.appointment_date).toLocaleDateString(
+        'en-GB',
+        {
+          day: '2-digit',
+          month: '2-digit',
+          year: 'numeric',
+        },
+      ),
+      kid_name: appoint.kid_name,
+      is_asd: appoint.is_asd ? 'ASD' : 'NON ASD',
+      parent_name: appoint.parent_name,
+      phone_no: appoint.phone_no,
+      email: appoint.email,
+      created_at: new Date(appoint.created_at).toLocaleDateString('en-GB', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric',
+      }),
+    };
+  });
+
+  stringify.stringify(formattedAppoints, { header: true }, (err, output) => {
+    console.log('check = ', output);
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Internal Server Error');
+    }
+
+    // Mengirimkan file CSV sebagai respons HTTP
+    res.attachment('janji_temu.csv');
+    res.send(output);
   });
 });
--- backend/backoffice/casdi/casdi.routes.js
+++ backend/backoffice/casdi/casdi.routes.js
@@ -1,9 +1,16 @@
 const express = require('express');
+
 const { getAllMembers, createMember } = require('./members.controller');
-const { createAppointment } = require('./casdi-appointment.controller');
+const {
+  createAppointment,
+  getHourMember,
+  downloadCasdi,
+} = require('./casdi-appointment.controller');
 const router = express.Router();
 
 router.route('/members').get(getAllMembers).post(createMember);
 router.route('/appointments').post(createAppointment);
+router.route('/appointments/hour').post(getHourMember);
+router.route('/appointments/download').get(downloadCasdi);
 
 module.exports = router;
